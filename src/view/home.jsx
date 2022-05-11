211ffd3f1bd4bba67cc7c7ec575dcbca8792a44a
vitor.souza <souza.vitorhugo@outlook.com>
2022-05-11 16:11:20 -0300

    #6 - Feat - Criação do slide dos banners e componentização.

--- src/view/home.jsx
+++ src/view/home.jsx
@@ -1,57 +1,15 @@
+import { Slideshow } from "../components/slide-show.jsx"
 import { Menu } from "../components/navigation.jsx"
-import { Carousel } from "react-bootstrap"
 
 export default function Home() {
     return(
+
         <page>
             <header className="page-header center-mid">
                 <Menu/>
             </header>
 
-            <section className="banners"> 
-
-            <Carousel className="carousel-main">
-
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={require('../assets/background/f1.jpeg')}
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={require('../assets/background/f2.jpeg')}
-                    alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={require('../assets/background/f3.jpeg')}
-                    alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-            </Carousel>
-            </section>
+            <Slideshow/>
         </page>
 
     )

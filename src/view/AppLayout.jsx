ea5a5435d55c2e47577030ec25673630f83e2c74
gustavomazzuco <gustavo_mazzuco@hotmail.com>
2022-06-22 20:51:40 -0300

    teste

--- src/view/AppLayout.jsx
+++ src/view/AppLayout.jsx
@@ -6,6 +6,7 @@ import Header from "../components/navigation";
 export default function AppLayout({ headerProps }) {
   return (
     <Fragment>
+      
       <Header {...headerProps} />
       <main id="content" role="main" className="container">
         <Outlet />
